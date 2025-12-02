27b2a0aef9ef903e6edfa23d6fe7275352f2ab6c
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-12-02 16:01:17 +0000

    switch case

--- js/app.js
+++ js/app.js
@@ -13,8 +13,16 @@ const updateContent = () => {
         case 'completed':
             content.innerHTML = 
             `
-                <h1>Active</h1>
-                <p>Bem vindo a active page
+                <h1>Completed</h1>
+                <p>Bem vindo a completed page</p>
             `;
+        break;
+        case 'all-todos':
+            content.innerHTML = 
+            `
+                <h1>All-todos</h1>
+                <p>Bem vindo a All-todos page</p>
+            `;
+        break;
     }
 }
