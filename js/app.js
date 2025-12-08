598fb60f7def211e1da65b8a3f011775dc3650b8
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-12-08 23:42:38 +0000

    js

--- js/app.js
+++ js/app.js
@@ -93,7 +93,6 @@ window.addEventListener('hashchange', updateContent);
 
 // Renderiza a view atual ao carregar o script (útil ao recarregar a página)
 updateContent();
-
 function setActive(item){
         //Get the element that was clicked allways the first
         const newActive = document.querySelector(`#${item}`)
